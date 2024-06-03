4eaa2d85128ff4aaf9966205165e4c941ed4c126
Angus Roberts-Thomson <angusrt64@gmail.com>
2024-06-03 13:19:56 +1000

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!\n";
+
+    std::string name;
+
+
+    std::cout<<"please enter your mums name";
+    std::cin>> name;
+    std::cout<<"im sorry but i hate your mum i think the name "<<name<<" is really dumb";
+    return 0;
+}
